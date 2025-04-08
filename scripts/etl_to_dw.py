a73f6c376a65da3a05041be3f6b466e3a7427c33
Houston Asher-Laws <houstonasherlaws@gmail.com>
2025-04-07 21:33:07 -0500

    changes to etl_to_dw.py

--- scripts/etl_to_dw.py
+++ scripts/etl_to_dw.py
@@ -17,13 +17,13 @@ def create_schema(cursor: sqlite3.Cursor) -> None:
     """Create tables in the data warehouse if they don't exist."""
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS customer  (
-            customer_id INTEGER PRIMARY KEY,
+            CustomerID INTEGER PRIMARY KEY,
             name TEXT,
             region TEXT,
             join_date TEXT,
-            LoyaltyPoints INTEGER,
-            PreferredContactMethod TEXT,
-            StandardDateTime TEXT
+            loyalty_points INTEGER,
+            preferred_contact_method TEXT,
+            standard_date_time TEXT
                   
         )
     """)
@@ -32,9 +32,10 @@ def create_schema(cursor: sqlite3.Cursor) -> None:
         CREATE TABLE IF NOT EXISTS product (
             product_id INTEGER PRIMARY KEY,
             product_name TEXT,
-            category TEXT
-            StockQuantity INTEGER,
-            Supplier TEXT
+            category TEXT,
+            unit_price REAL,
+            stock_quantity INTEGER,
+            supplier TEXT
         )
     """)
     
@@ -43,8 +44,13 @@ def create_schema(cursor: sqlite3.Cursor) -> None:
             sale_id INTEGER PRIMARY KEY,
             customer_id INTEGER,
             product_id INTEGER,
+            store_id INTEGER,
+            campaign_id INTEGER,
+            discount_percent REAL,
             sale_amount REAL,
+            payment_type TEXT,
             sale_date TEXT,
+            promotion TEXT,
             quantity INTEGER,
             FOREIGN KEY (customer_id) REFERENCES customer (customer_id),
             FOREIGN KEY (product_id) REFERENCES product (product_id)
@@ -56,6 +62,8 @@ def delete_existing_records(cursor: sqlite3.Cursor) -> None:
     cursor.execute("DELETE FROM customer")
     cursor.execute("DELETE FROM product")
     cursor.execute("DELETE FROM sale")
+
+
 
 def insert_customers(customers_df: pd.DataFrame, cursor: sqlite3.Cursor) -> None:
     """Insert customer data into the customer table."""
@@ -83,6 +91,32 @@ def load_data_to_db() -> None:
         customers_df = pd.read_csv(PREPARED_DATA_DIR.joinpath("customers_data_prepared.csv"))
         products_df = pd.read_csv(PREPARED_DATA_DIR.joinpath("products_data_prepared.csv"))
         sales_df = pd.read_csv(PREPARED_DATA_DIR.joinpath("sales_data_prepared.csv"))
+
+         # Rename columns in the DataFrame to match the database schema
+        #customers_df.rename(columns={"CustomerID": "customer_id"}, inplace=True)
+        customers_df.rename(columns={"JoinDate": "join_date"}, inplace=True)
+        customers_df.rename(columns={"LoyaltyPoints": "loyalty_points"}, inplace=True)
+        customers_df.rename(columns={"PreferredContactMethod": "preferred_contact_method"}, inplace=True)
+        customers_df.rename(columns={"StandardDateTime": "standard_date_time"}, inplace=True)
+        # Rename columns in the DataFrame to match the database schema
+        products_df.rename(columns={"ProductID": "product_id"}, inplace=True)
+        products_df.rename(columns={"ProductName": "product_name"}, inplace=True)
+        products_df.rename(columns={"Category": "category"}, inplace=True)
+        products_df.rename(columns={"UnitPrice": "unit_price"}, inplace=True)
+        products_df.rename(columns={"StockQuantity": "stock_quantity"}, inplace=True)
+        products_df.rename(columns={"Supplier": "supplier"}, inplace=True)
+        # Rename columns in the DataFrame to match the database schema
+        sales_df.rename(columns={"TransactionID": "sale_id"}, inplace=True)
+        sales_df.rename(columns={"CustomerID": "customer_id"}, inplace=True)
+        sales_df.rename(columns={"ProductID": "product_id"}, inplace=True)
+        sales_df.rename(columns={"StoreID": "store_id"}, inplace=True)
+        sales_df.rename(columns={"CampaignID": "campaign_id"}, inplace=True)
+        sales_df.rename(columns={"Discount percent": "discount_percent"}, inplace=True)
+        sales_df.rename(columns={"CampaignID": "campaign_id"}, inplace=True)
+        sales_df.rename(columns={"SaleAmount": "sale_amount"}, inplace=True)
+        sales_df.rename(columns={"SaleDate": "sale_date"}, inplace=True)
+        sales_df.rename(columns={"Quantity": "quantity"}, inplace=True)
+        sales_df.rename(columns={"Payment Type": "payment_type"}, inplace=True)
 
         # Insert data into the database
         insert_customers(customers_df, cursor)
